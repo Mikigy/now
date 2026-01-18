a7ba168c6d694010bffe533eac7003bbcfa22c80
Mikiale Goitom <73868256+Mikigy@users.noreply.github.com>
2026-01-18 04:30:14 -0800

    Create history.h
    yes

---
+++ history.h
@@ -0,0 +1,10 @@
+#ifndef HISTORY_H
+#define HISTORY_H
+
+void viewHistory(int* id, char name[][20], char color[][20],
+                 char location[][20], char date[][15],
+                 char reporter[][30], char claimer[][30],
+                 char dateReported[][15], char dateClaimed[][15],
+                 int* claimed);
+
+#endif
